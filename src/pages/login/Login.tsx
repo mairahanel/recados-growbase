a56b04c15647406b875a154e8887fd5cd9a8a5d0
Maira Joana Hanel <maira.joana.hanel@gmail.com>
2022-08-29 16:50:55 -0300

    layout_login-cadastro

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,156 @@
+import { AccountCircle } from "@mui/icons-material"
+import { Box, Button, Card, CardActions, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, InputAdornment, TextField, Typography } from "@mui/material"
+import { useState } from "react";
+
 export const Login: React.FC = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const openModal = () => {
+    setOpen(true);
+    };
+
+    const closeModal = () => {
+    setOpen(false);
+     };
+
+
+
     return(
-        <>
-            <h1>Hello world</h1>
-        </>
+        <Grid container component='main' sx={{ backgroundColor: "#dda0dd" }}>
+            <Grid item 
+                xs={12} 
+                height='100vh' 
+                sx={{ 
+                display: "flex", 
+                justifyContent: "center", 
+                alignItems: "center" 
+                }}>
+                <Box>
+                    <Card 
+                    sx={{ 
+                    display: "flex", 
+                    alignItems: "center", 
+                    justifyContent: "center", 
+                    height: "500px", 
+                    width: "600px",
+                    }}>
+                        <Grid container 
+                        height='50vh' 
+                        sx={{ 
+                        display: "flex", 
+                        justifyContent: "center" 
+                        }}>
+                        <Grid item xs={12} 
+                        sx={{ 
+                        display: "flex", 
+                        justifyContent: "center", 
+                        flexDirection: "row", 
+                        alignItems: "start" }}>
+                            <Typography variant='h5'>Faça seu login!</Typography>
+                        </Grid>
+                        <CardActions>
+                            <Grid container 
+                            sx={{ 
+                            display: "flex", 
+                            justifyContent: "center" 
+                            }}>
+                                <Grid item xs={12}>
+                                    <TextField 
+                                        variant='outlined' 
+                                        label='E-mail' 
+                                        type='email'
+                                        sx={{ 
+                                        width: "500px", 
+                                        marginTop: "40px" 
+                                    }}
+                                        InputProps={{
+                                            endAdornment: (
+                                            <InputAdornment position="start">
+                                                <AccountCircle />
+                                            </InputAdornment>
+                                            ),
+                                        }}>
+                                    </TextField>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField 
+                                        variant='outlined' 
+                                        label='Senha' 
+                                        type='password'
+                                        sx={{ 
+                                        width: "500px", 
+                                        marginTop: "20px" 
+                                        }} ></TextField>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button 
+                                        variant='contained' 
+                                        sx={{ 
+                                        width: "280px", 
+                                        marginTop: "50px", 
+                                        backgroundColor: "#00FF7F", 
+                                        color: "black" 
+                                        }}>Entrar</Button>
+                                </Grid>
+                                <Grid item 
+                                    xs={12} 
+                                    marginTop='20px' 
+                                    fontSize='medium'>
+                                    <Typography variant='subtitle1'>ou</Typography>
+                                    <Typography variant='subtitle2'>
+                                        <Button onClick={openModal} sx={{ color:'black' }}>Cadastre-se</Button>
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </CardActions>
+                        </Grid>
+                    </Card>
+                </Box>
+            </Grid>
+            <div>
+                <Dialog open={open} onClose={closeModal}>
+                <DialogTitle sx={{ 
+                            display: "flex", 
+                            justifyContent: "center" 
+                            }}>
+                                Crie sua conta</DialogTitle>
+                <DialogContent>
+                <DialogContentText>
+                    Para criar uma nova conta, preencha corretamente todos os campos abaixo.
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="E-mail"
+                    type="email"
+                    fullWidth
+                    variant="outlined"
+                    sx={{ marginTop: "30px" }}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Nova senha"
+                    type="password"
+                    fullWidth
+                    variant="outlined"
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Repita sua senha"
+                    type="password"
+                    fullWidth
+                    variant="outlined"
+                />
+                </DialogContent>
+                <DialogActions>
+                <Button variant='outlined' onClick={closeModal}>Fechar</Button>
+                <Button variant='contained' onClick={closeModal}>Cadastrar</Button>
+                </DialogActions>
+                </Dialog>
+            </div>
+        </Grid>
     )
 }
